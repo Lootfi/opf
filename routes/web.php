fc01c0186aaad11fbc0d535f04b087bedbb32a1b
Lootfi <lotfiholmes@hotmail.com>
2021-05-04 19:23:58 +0100

    adding laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Auth::routes(['register' => false]);
+Auth::routes(['register' => false]);
