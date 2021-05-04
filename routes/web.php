6446b6f8fec4571c7a14bbf1816f2a723c103e0d
Lootfi <lotfiholmes@hotmail.com>
2021-05-04 19:40:36 +0100

    setting up auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
