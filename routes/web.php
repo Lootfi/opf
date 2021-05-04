002c390ffc079cdd3001c849a8528ce0fecc3178
Lootfi <lotfiholmes@hotmail.com>
2021-05-04 19:53:03 +0100

    revert to work with jetstream instead

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes(['register' => false]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Auth::routes(['register' => false]);
